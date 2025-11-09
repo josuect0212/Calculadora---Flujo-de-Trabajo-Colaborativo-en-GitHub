5fa5fcbd76953600fbb8cc1774def89bd72664c8
Keilyn Valeria Garita González <kgaritagonzalez@gmail.com>
2025-11-09 17:58:03 -0600

    feat: Implementar impuesto de venta 15%

--- calculadora.py
+++ calculadora.py
@@ -6,7 +6,7 @@ def aplicar_reglas_compra(precio):
     """
     
     # Ambos desarrolladores DEBEN MODIFICAR la línea inmediatamente debajo de este comentario.
-    precio = precio * 0.95
+    precio = precio * 1.15
     
     return precio
 
