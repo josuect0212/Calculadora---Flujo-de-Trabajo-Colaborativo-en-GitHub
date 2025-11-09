5454f5b88bae791fe18cb3d3f091a156d6d78eeb
josuect0212 <joscalvo@estudiantec.cr>
2025-11-08 23:20:30 -0600

    Se añade el archivo calculadora.py inicial con la función aplicar_reglas_compra
    Se introduce el archivo calculadora.py el cual contiene la función aplicar_reglas_compra, que se encarga de aplicar las reglas de negocio al precio de compra. Además, se incluye un test de ejecución como demostración.

---
+++ calculadora.py
@@ -0,0 +1,17 @@
+# calculadora.py
+
+def aplicar_reglas_compra(precio):
+    """
+    Aplica todas las reglas de negocio al precio de compra.
+    """
+    
+    # Ambos desarrolladores DEBEN MODIFICAR la línea inmediatamente debajo de este comentario.
+    precio = precio
+    
+    return precio
+
+# Inicia la ejecución de prueba
+if __name__ == '__main__':
+    precio_inicial = 100
+    precio_final = aplicar_reglas_compra(precio_inicial)
+    print(f"Precio inicial: {precio_inicial}. Precio final: {precio_final}")
